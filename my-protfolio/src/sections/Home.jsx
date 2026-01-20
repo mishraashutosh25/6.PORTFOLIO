02283e4433c096882b1292d657efe14430535a53
ashutosh_mishra2812 <2k23.csaiml2313890@gmail.com>
2026-01-20 19:55:20 +0530

    fixed the error of resume button not downloading the file

--- my-protfolio/src/sections/Home.jsx
+++ my-protfolio/src/sections/Home.jsx
@@ -102,7 +102,7 @@ export default function Home() {
 
             {/* RESUME BUTTON */}
             <a
-              href="/Resume.pdf"
+              href="Resume.pdf"
               download
               className="cursor-pointer px-8 py-3 rounded-full text-black bg-white font-medium shadow-lg hover:bg-gray-300 hover:scale-105 transition-transform"
             >
