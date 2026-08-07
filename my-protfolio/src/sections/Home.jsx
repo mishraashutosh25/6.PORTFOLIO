76e6df01ef2ff4281198af0844c79ccffd560640
Ashutosh mishra <ashutoshmishra.dev25@gmail.com>
2026-08-08 00:44:19 +0530

    Update resume part

--- my-protfolio/src/sections/Home.jsx
+++ my-protfolio/src/sections/Home.jsx
@@ -102,8 +102,8 @@ export default function Home() {
 
             {/* RESUME BUTTON */}
             <a
-              href="Ashutosh-resume.pdf"
-              download="Ashutosh-resume.pdf"
+              href="Ashutosh-Resume.pdf"
+              download="Ashutosh-Resume.pdf"
               className="cursor-pointer px-8 py-3 rounded-full text-black bg-white font-medium shadow-lg hover:bg-gray-300 hover:scale-105 transition-transform"
             >
               My Resume
